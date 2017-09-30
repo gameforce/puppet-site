a3712accfb585a76d60fa1d0946330fe141c9c0c
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-09-29 18:45:12 -0700

    add common.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,49 +1,5 @@
 node default {
-  # mod 'razorsedge-openvmtools', '1.1.0'
-  include ::openvmtools
-  
-  class { '::openvmtools':
-    with_desktop => filse,
-  }
-  
-  # mod 'saz-motd', '2.4.0'
-  class { 'motd': }
-
-  # mod 'puppetlabs-ntp', '6.2.0'
-  class { '::ntp':
-    servers => [ 'clock.stellarcreative.lab' ],
-  }
-  
-  # Disable ipv6 via sysctl - this needs dracut -f if it breaks rpcbind
-  # TODO: move disabling ipv6 to kickstart
-  sysctl { 'net.ipv6.conf.all.disable_ipv6':
-    value => '1' }
-  
-  # mod 'puppetlabs-firewall', '1.9.0' 
-  class { 'firewall':
-    ensure => 'stopped',
-  }
-
-  # mod 'saz-sudo', '4.2.0'
-  class { 'sudo': }
-  sudo::conf { 'systems':
-    priority =>   10,
-    source   =>   'puppet:///files/sudo/systems.conf',
-  }
-
-  # mod 'yuav-autofs', '1.2.4'
-  class { 'autofs':
-    mount_files => {
-      net => {
-         mountpoint  => '/net',
-         file_source => 'puppet:///files/autofs/auto.net.data',
-       },
-      job => {
-         mountpoint  => '/job',
-         file_source => 'puppet:///files/autofs/auto.job.data',
-        }
-      }
-    }
+  include common
   }
 
 node kam1.stellarcreative.lab {
