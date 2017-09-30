5cc5f01c39d2452fd4f8cdf2c6022dfe25db5834
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-09-29 18:15:34 -0700

    add firewall class to kam1 node

--- manifests/site.pp
+++ manifests/site.pp
@@ -49,13 +49,18 @@ node default {
 }
 
 node kam1.stellarcreative.lab {
+  # mod 'puppetlabs-firewall', '1.9.0' 
+    class { 'firewall':
+      ensure =>  'stopped',
+  }
+            
   # mod 'puppetlabs-puppetdb', '6.0.1'
   # Configure puppetdb and its underlying database
   class { 'puppetdb': }
 
   # Configure the Puppet master to use puppetdb
   class { 'puppetdb::master::config': }
-}
+  }
 
 node box49.stellarcreative.lab {
 }
