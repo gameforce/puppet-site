8f559a8909a73f843f154920f39e5e07a44afe80
Nick <nick@gameforce.net>
2017-10-02 15:11:59 -0700

    remove firewall from site as it is already in common

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,12 +2,12 @@ node default {
   include common
   }
 
-node kam1.stellarcreative.lab {
-  # mod 'puppetlabs-firewall', '1.9.0' 
-  class { 'firewall':
-    ensure =>  'stopped',
-  }
-            
+#node kam1.stellarcreative.lab {
+  # mod 'puppetlabs-firewall', '1.9.0'
+#  class { 'firewall':
+#    ensure =>  'stopped',
+#  }
+
   # mod 'puppetlabs-puppetdb', '6.0.1'
   # Configure puppetdb and its underlying database
   class { 'puppetdb': }
