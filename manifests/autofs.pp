17fb52b0ab136ddb499fc68f69cdd472224f9f5d
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-10-02 17:31:53 -0700

    duplicate net entry derp

--- manifests/autofs.pp
+++ manifests/autofs.pp
@@ -1,14 +1,14 @@
 # mod 'puppet-autofs', '4.0.0'
-#autofs::mount { 'net':
-#  mount       => '/net',
-#  mapfile     => '/etc/auto.net',
-#  mapcontents => ['* -user,rw,soft,intr,rsize=32768,wsize=32768,tcp,nfsvers=3,noacl srv1:/data/net'],
-#  options     => '--timeout=300',
-#  order       => 01
-#}
+autofs::mount { 'net':
+  mount       => '/net',
+  mapfile     => '/etc/auto.net',
+  mapcontents => ['* -user,rw,soft,intr,rsize=32768,wsize=32768,tcp,nfsvers=3,noacl srv1:/data/net'],
+  options     => '--timeout=300',
+  order       => 01
+}
 
 autofs::mount { 'job':
-  mount       => '/net',
+  mount       => '/job',
   mapfile     => '/etc/auto.job',
   mapcontents => ['* -user,rw,soft,intr,rsize=32768,wsize=32768,tcp,nfsvers=3,noacl srv1:/data/job'],
   options     => '--timeout=300',
