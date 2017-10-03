539f6790748d36a46d41d73ea0cdd1d68133bc2b
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-10-03 09:58:00 -0700

    rename mounts.pp

---
+++ manifests/mounts.pp
@@ -0,0 +1,25 @@
+class mounts {
+  # enable browse mode in autofs.conf
+  file_line { 'browse_mode':
+    path  => '/etc/autofs.conf',
+    line  => 'browse_mode = "yes"',
+    match => '^browse_mode*',
+  }
+
+  # mod 'puppet-autofs', '4.0.0'
+  autofs::mount { 'net':
+    mount       => '/net',
+    mapfile     => '/etc/auto.net',
+    mapcontents => ['* -user,rw,soft,intr,rsize=32768,wsize=32768,tcp,nfsvers=3,noacl srv1:/data/net'],
+    options     => '--timeout=300',
+    order       => 01
+  }
+
+  autofs::mount { 'job':
+    mount       => '/job',
+    mapfile     => '/etc/auto.job',
+    mapcontents => ['* -user,rw,soft,intr,rsize=32768,wsize=32768,tcp,nfsvers=3,noacl srv1:/data/job'],
+    options     => '--timeout=300',
+    order       => 02
+  }
+}
