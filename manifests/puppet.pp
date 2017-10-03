07ebe20752cdaf7ddf8df1be1308511faed3c273
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-10-03 10:22:18 -0700

    actually add puppet.pp

---
+++ manifests/puppet.pp
@@ -0,0 +1,10 @@
+class puppet {
+
+  # mod 'puppetlabs-puppetdb', '6.0.1'
+  # Configure puppetdb and its underlying database
+  class { 'puppetdb': }
+
+  # Configure the Puppet master to use puppetdb
+  class { 'puppetdb::master::config': }
+}
+
