7e7f5f5fb6a0cc83a8e247f7b723f723f041f598
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-10-05 13:39:28 -0700

    add packages profile

---
+++ site/profile/manifests/packages.pp
@@ -0,0 +1,12 @@
+# packages installed by puppet
+class profile::packages {
+  package { 'epel-release': ensure => 'installed', }
+  package { 'bind-utils': ensure => 'installed', }
+  package { 'nfs-utils': ensure => 'installed', }
+  package { 'nmap': ensure => 'installed', }
+  package { 'zsh': ensure => 'installed', }
+  package { 'screen': ensure => 'installed', }
+  package { 'vim-enhanced': ensure => 'installed', }
+  package { 'wget': ensure => 'installed', }
+  package { 'htop': ensure => 'installed', }
+}
