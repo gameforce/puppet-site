3b6057a1a0a9347383617e5b08b354b35dc64613
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-10-05 10:24:04 -0700

    add accounts profile

---
+++ site/profile/manifests/accounts.pp
@@ -0,0 +1,22 @@
+# requires vcsrepo from the forge
+class profile::accounts {
+
+  group { 'puppet':
+    ensure => 'present',
+    gid    => '52',
+  }
+
+  # create our default local user
+  user { 'systems':
+    ensure           => 'present',
+    home             => '/home/systems',
+    managehome       => 'true',
+    # Generate password with openssl passwd -1
+    password         => '$1$VVrr57Wd$2gyRbqpQubfMfF1FCKUxf1',
+    password_max_age => '99999',
+    password_min_age => '0',
+    shell            => '/bin/bash',
+    uid              => '502',
+    comment          => 'systems',
+    groups           => [ 'users','puppet' ]
+  }
