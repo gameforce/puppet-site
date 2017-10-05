70f1799b8ed77d28fb330ec9400248956d6108d0
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-10-05 15:05:41 -0700

    ensure 'present' and add the commas

--- site/profile/manifests/repo.pp
+++ site/profile/manifests/repo.pp
@@ -1,8 +1,8 @@
 class profile::repo {
   # kickstart setup
   file { '/var/www/html/vhosts/repo/ks/centos-desktop.ks'
-    ensure => 'file'
-    owner  => 'root'
+    ensure =>  'present',
+    owner  => 'root',
     group  => 'root',
     mode   => '0755',
     source => 'puppet:///files/kickstart/centos-desktop.ks',
