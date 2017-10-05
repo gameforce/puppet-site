7159794a2a3cb57f1b20f49bb63d6a63ef8f9d98
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-10-05 13:33:13 -0700

    rename mounts to autofs and add autofs.conf bits

--- site/profile/manifests/mounts.pp
+++ site/profile/manifests/mounts.pp
@@ -1,18 +1,21 @@
-class profile::mounts { 
+class profile::autofs { 
   # enable browse mode in autofs.conf
-  file_line { 'browse_mode':
-    path  => '/etc/autofs.conf',
-    line  => 'browse_mode = "yes"',
-    match => '^browse_mode*',
+  file { '/etc/autofs.conf':
+    ensure          =>  'present',
+    owner           =>  'root',
+    group           =>  'root',
+    mode            =>  '0644',
+    source          =>  'puppet:///files/autofs/autofs.conf',
+    notify          =>  Service['autofs'],
   }
 
   class { 'autofs':
-    mount_files => {
-      job  => {
+    mount_files     => {
+      job           => {
         mountpoint  => '/job',
         file_source => 'puppet:///files/autofs/auto.job',
       },
-      net  => {
+      net           => {
         mountpoint  => '/net',
         file_source => 'puppet:///files/autofs/auto.net',
       }
