1de8234f51c31b7d63f6cd46379aa3d9ea01cde1
Nick <nick.gotsinas@stellarcreativelab.com>
2017-10-11 15:21:45 -0700

    move to a subscribe type

--- site/profile/manifests/ssh.pp
+++ site/profile/manifests/ssh.pp
@@ -2,11 +2,14 @@ class profile::ssh {
   
   # disable strict host checking in ssh
   file { '/etc/ssh/sshd_config':
-    ensure => 'present',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0600',
-    source => 'puppet:///files/ssh/sshd_config',
-    notify => Service["sshd"],
+    ensure    => 'present',
+    owner     => 'root',
+    group     => 'root',
+    mode      => '0600',
+    source    => 'puppet:///files/ssh/sshd_config',
   }
+
+  service { sshd:
+    ensure    =>  running,
+    subscribe =>  File["/etc/sshd_config"],
 }
