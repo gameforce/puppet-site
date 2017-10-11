39b4d79ce7e25bf9afa5fa1c833bb2755bd584a0
Nick <nick.gotsinas@stellarcreativelab.com>
2017-10-11 15:54:25 -0700

    testing conditionals

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -19,9 +19,10 @@ class profile::base {
   }
 
   # Disable ipv6 via sysctl run dracut -f if it breaks rpcbind
+  class disable_ipv6 {
   if "test -e /proc/sys/net/ipv6/conf/all/disable_ipv6"
-  sysctl { 'net.ipv6.conf.all.disable_ipv6':
-    value  => '1' 
+  sysctl { 'net.ipv6.conf.all.disable_ipv6': value  => '1' }
+  else notify {ipv6 disabled on boot}
   }
 
   # mod 'puppetlabs-firewall', '1.9.0'
