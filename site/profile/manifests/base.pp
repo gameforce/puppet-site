81d24006c55d583ea8c14a735bd827999b9a49b4
Nick <nick.gotsinas@stellarcreativelab.com>
2017-10-11 13:03:29 -0700

    add service restart nofity for sshd

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -12,11 +12,12 @@ class profile::base {
   
   # disable strict host checking in ssh
   file { '/etc/ssh/sshd_config':
-    ensure  => 'present',
-    owner   => 'root',
-    group   => '0',
-    mode    => '0600',
-    source  => 'puppet:///files/ssh/sshd_config',
+    ensure => 'present',
+    owner  => 'root',
+    group  => '0',
+    mode   => '0600',
+    source => 'puppet:///files/ssh/sshd_config',
+    notify =>   Service['sshd'],
   } 
 
 
