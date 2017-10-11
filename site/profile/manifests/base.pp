e80f953ca996286bea57067e8406a68790cb1b0d
Nick <nick.gotsinas@stellarcreativelab.com>
2017-10-11 13:11:30 -0700

    turn it into a class

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -11,14 +11,16 @@ class profile::base {
   }
   
   # disable strict host checking in ssh
-  file { '/etc/ssh/sshd_config':
-    ensure => 'present',
-    owner  => 'root',
-    group  => '0',
-    mode   => '0600',
-    source => 'puppet:///files/ssh/sshd_config',
-  } 
-    notify => Service['sshd'],
+  class { 'ssh_config':
+    file { '/etc/ssh/sshd_config':
+      ensure => 'present',
+      owner  => 'root',
+      group  => '0',
+      mode   => '0600',
+      source => 'puppet:///files/ssh/sshd_config',
+      notify => Service['sshd'],
+    }
+  }
 
   # mod 'saz-motd', '2.4.0'
   class { 'motd': }
