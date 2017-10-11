c9260f848a9400d98de615ef0bebfbdd4477eb3a
Nick <nick.gotsinas@stellarcreativelab.com>
2017-10-11 16:43:11 -0700

    replce syctl module

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -19,11 +19,8 @@ class profile::base {
   }
 
   # Disable ipv6 via sysctl run dracut -f if it breaks rpcbind
-  if exec{"test -e /proc/sys/net/ipv6/conf/all/disable_ipv6"} {
-    sysctl { 'net.ipv6.conf.all.disable_ipv6': value  => '1' }
-  }
-  else {
-    warning('ipv6 was disabled from kickstart on this host.')
+  sysctl::value { 'net.ipv6.conf.all.disable_ipv6': 
+      value  => '1' 
   }
 
   # mod 'puppetlabs-firewall', '1.9.0'
