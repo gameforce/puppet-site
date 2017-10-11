a63604841a3bc0339c8c393b23f98840f0bcdd3d
Nick <nick.gotsinas@stellarcreativelab.com>
2017-10-11 12:43:36 -0700

    deploy r10k sshkeys

--- site/profile/manifests/accounts.pp
+++ site/profile/manifests/accounts.pp
@@ -21,20 +21,20 @@ class profile::accounts {
     groups           => [ 'users','puppet' ]
   }
 
-  file { '/home/systems/.ssh/id_rsa':
+  file { '/root/.ssh/id_rsa':
     ensure          =>  'present',
-    owner           =>  'systems',
-    group           =>  'users',
+    owner           =>  'root',
+    group           =>  '0',
     mode            =>  '0600',
     source          =>  'puppet:///files/ssh/id_rsa',
   }
 
-  ssh_authorized_key { 'systems@stellar': 
-    user             =>  'systems',
+  ssh_authorized_key { 'r10k@stellar': 
+    user             =>  'root',
     ensure           =>  present, 
     type             =>  'ssh-rsa', 
-    key              =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQDXA3azLpA7ZBtfud+iul+h8iP2f2TcFYIWZn4/z8ofIT8ikmyElA0eTrpo2lE+ykv62lywiM3O/b3QUDMlELhJXXLbglrxX/El9Yw9KQjz0P5KgmAiCuUKCntqdUDSDLPb24spUR2zG/oUgah0SY2DZqS9vTi9iDKKV/CWEtRHakLiydB8OqBX58834PwV348COMk9/CC2kMWHFYUHjIqx5vr3GsxXbTBIlr5KeWZ9iQeYa4UrlHn4FZ1o8FWKucOFFhtGaTyQ9r+USZ4AgswelYjSeGLizHkM0zrtJdboq9F+gofGF3DOxq7Jk+bxJ4yhrrhNfU1ygzkjdLVQshzd',
-    name             =>  'systems@stellar',
+    key              =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQC6WN9RbxpLn9oa3IArG6MVT4F7BySNyjKp10itqC38qqNkeEYy8oAHjHh56ErnmrONZ1OomKALaRblJypRo8jBqcULsn3B4R0NW37vMLCyCulk/YasyMiDtU+yE74gkIbTUkbV2Q8t2PHgq69aqKVs2cgXC3znvd93yjqSJEgUjQjzWDiHifs/BTWEFWFSQ1VpQoaKlRueN0048pXC3u6QDldmo82bJunW6FSkq7fkim+ADSOpT/ptxO0AkEetgflvB1cSh1Ar+eyOU0ljljBLp0D4ltjL2UU1HnwXn7o1CRW0JEyz/51PFK6OnwcgocGzmay3b5qd6Y6oPHznqCUp',
+    name             =>  'r10k@stellar',
   }
 
   # clone the dotfiles repo
