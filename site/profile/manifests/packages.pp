75d49a59083498b97b01674587e82b5a38982c2d
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-10-24 15:39:48 -0700

    add elrepo

--- site/profile/manifests/packages.pp
+++ site/profile/manifests/packages.pp
@@ -2,6 +2,8 @@
 class profile::packages {
 
   # yum repos that puppet will manage
+
+  # ius repo and package for python3
   yumrepo { 'ius':
     baseurl => 'https://dl.iuscommunity.org/pub/ius/stable/CentOS/7/$basearch',
     descr   => 'ius',
@@ -10,9 +12,22 @@ class profile::packages {
   }
   package { python36u:
     ensure => present,
-   # require => Yumrepo["ius"],
+    require => Yumrepo["ius"],
+  }
+  
+  # elrepo for nvidia driver
+  yumrepo { 'elrepo':
+    baseurl => 'http://elrepo.org/linux/elrepo/el7/$basearch/',
+    descr   => 'ELRepo.org Community Enterprise Linux Repository - el7',
+    gpgcheck => '0',
+    enabled => '1',
+  }
+  package { kmod-nvidia:
+    ensure => present,
+    require => Yumrepo["elrepo"],
   }
 
+  # common packages
   package { 'epel-release': ensure => 'installed', }
   package { 'bind-utils': ensure => 'installed', }
   package { 'nfs-utils': ensure => 'installed', }
