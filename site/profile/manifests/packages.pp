08c9f2a39984984a96f9b14be633b35a0d3d11fa
Nick Gotsinas <nick.gotsinas@box53.stellarcreative.lab>
2017-11-24 11:33:49 -0800

    conditional package installs

--- site/profile/manifests/packages.pp
+++ site/profile/manifests/packages.pp
@@ -6,31 +6,32 @@ class profile::packages {
   if $facts['systype'] == 'desktop' {
     #notice ('This is a desktop $1')
     notify {"This is a $systype" :}
+    notify {"Installing desktop packages :"}
+    # ius repo and package for python3
+    yumrepo { 'ius':
+      baseurl => 'https://dl.iuscommunity.org/pub/ius/stable/CentOS/7/$basearch',
+      descr   => 'ius',
+      gpgcheck => '0',
+      enabled => '1',
+    }
+    package { python36u:
+      ensure => present,
+      require => Yumrepo["ius"],
+    }
   }
   elsif $facts['systype'] == 'server' {
-    #notice ('This is not a server $systype $1')
-    notify {"this is a server $1" :}
+    notify {"this is a server" :}
+    notify {"Installing server packages" :}
   }
-
-  # ius repo and package for python3
-  yumrepo { 'ius':
-    baseurl => 'https://dl.iuscommunity.org/pub/ius/stable/CentOS/7/$basearch',
-    descr   => 'ius',
-    gpgcheck => '0',
-    enabled => '1',
+  else {
+    # install common packages
+    package { 'epel-release': ensure => 'installed', }
+    package { 'bind-utils': ensure => 'installed', }
+    package { 'nfs-utils': ensure => 'installed', }
+    package { 'nmap': ensure => 'installed', }
+    package { 'zsh': ensure => 'installed', }
+    package { 'screen': ensure => 'installed', }
+    package { 'vim-enhanced': ensure => 'installed', }
+    package { 'htop': ensure => 'installed', }
   }
-  package { python36u:
-    ensure => present,
-    require => Yumrepo["ius"],
-  }
-
-  # common packages
-  package { 'epel-release': ensure => 'installed', }
-  package { 'bind-utils': ensure => 'installed', }
-  package { 'nfs-utils': ensure => 'installed', }
-  package { 'nmap': ensure => 'installed', }
-  package { 'zsh': ensure => 'installed', }
-  package { 'screen': ensure => 'installed', }
-  package { 'vim-enhanced': ensure => 'installed', }
-  package { 'htop': ensure => 'installed', }
 }
