9eaa96df58b8602a0a80743e4639afa4af45b8a1
Nick Gotsinas <nick.gotsinas@box53.stellarcreative.lab>
2017-11-24 13:44:17 -0800

    newline #2

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -6,7 +6,7 @@ class profile::base {
   # setup environment
   $stellarpath = '/net/software/bin:/usr/lib64/qt-3.3/bin:/opt/puppetlabs/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin'
   file { "/etc/profile.d/stellar.sh":
-    content => "export PATH=${stellarpath\n}",
+    content => "export PATH=${stellarpath}\n",
     mode    => '0644'
   }
 
