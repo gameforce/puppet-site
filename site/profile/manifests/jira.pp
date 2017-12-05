faddce76765fc51afef58cf21837fa20d3ef387c
Nick Gotsinas <nick.gotsinas@box53.stellarcreative.lab>
2017-12-05 12:39:51 -0800

    create user for psql

--- site/profile/manifests/jira.pp
+++ site/profile/manifests/jira.pp
@@ -3,6 +3,11 @@
 class profile::jira {
   include postgresql::server
 
+    postgresql::database_user{'jiraadm':
+    password_hash => 'mypassword',
+  }
+
+
   class { 'jira':
     javahome    => '/usr',
     deploy_module => 'archive',
