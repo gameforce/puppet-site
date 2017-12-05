d5acc7485864ba7a8f725f6483567d6c3b6f5683
Nick Gotsinas <nick.gotsinas@box53.stellarcreative.lab>
2017-12-05 12:48:06 -0800

    needed space

--- site/profile/manifests/jira.pp
+++ site/profile/manifests/jira.pp
@@ -2,7 +2,7 @@
 
 class profile::jira {
   include postgresql::server
-  include postgresql::role{'jiraadm':
+  include postgresql::role {'jiraadm':
     password_hash => '34819d7beeabb9260a5c854bc85b3e44',
     createdb => 'true',
   }
