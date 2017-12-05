39fd67437c65fb2e26775adf72527555ae5254e9
Nick Gotsinas <nick.gotsinas@box53.stellarcreative.lab>
2017-12-05 12:56:28 -0800

    try passing params from hiera

--- site/profile/manifests/jira.pp
+++ site/profile/manifests/jira.pp
@@ -2,12 +2,6 @@
 
 class profile::jira {
   include postgresql::server
-  include postgresql::role
-  postgresql::role {'jiraadm':
-    password_hash => '34819d7beeabb9260a5c854bc85b3e44',
-    createdb => 'true',
-  }
-
 
   class { 'jira':
     javahome    => '/usr',
