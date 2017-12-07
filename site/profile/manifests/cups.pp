4c34332b91da63051dfb70ebd0e4b9863e88e56a
Nick Gotsinas <nick.gotsinas@box53.stellarcreative.lab>
2017-12-07 12:44:31 -0800

    try accepting to turn on the queue

--- site/profile/manifests/cups.pp
+++ site/profile/manifests/cups.pp
@@ -4,14 +4,15 @@ class profile::cups {
 include '::cups'
 
   cups_queue { 'WF-3720':
-    ensure => 'printer',
-    enabled => 'true',
-    ppd    => '/usr/share/cups/model/WF-3720.ppd',
-    uri    => 'lpd://172.16.21.37:515/PASSTHRU'
+    ensure    => 'printer',
+    enabled   => 'true',
+    accepting => 'true',
+    ppd       => '/usr/share/cups/model/WF-3720.ppd',
+    uri       => 'lpd://172.16.21.37:515/PASSTHRU'
   }
 
   file { '/usr/share/cups/model/WF-3720.ppd':
-    ensure => 'file',
-    source => 'puppet:///files/cups/WF-3720.ppd'
+    ensure    => 'file',
+    source    => 'puppet:///files/cups/WF-3720.ppd'
   }
 }
