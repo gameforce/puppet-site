fd0ba484124cb3269c6dcc4e065d947ae674269e
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-12-11 18:24:55 -0800

    include yum in base

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,6 +2,7 @@ class profile::base {
 
   # includes
   include ::openvmtools
+  include yum
 
   # stellar environment and repo
   $stellarpath = '/net/software/bin:/usr/lib64/qt-3.3/bin:/opt/puppetlabs/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin'
