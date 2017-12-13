91ddae1e8f37615d2c258df0fe5c0724b4cf5a95
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-12-13 10:27:48 -0800

    test hiera

--- site/profile/manifests/yum.pp
+++ site/profile/manifests/yum.pp
@@ -2,20 +2,11 @@ class profile::yum {
 
 #include 'yum'
 
-class { 'yum':
+  class { 'yum':
     managed_repos =>  [ 'stellar' ],
-  }
+   }
 
-yum::repos { 'stellar':
-    ensure    => 'present',
-    enabled   => 'true',
-    descr     => 'Stellar Repo',
-    baseurl   => 'http://repo/stellar/x86_64/',
-    gpgcheck  => false,
-    target    => '/etc/yum.repos.d/stellar.repo'
-  }
-
-  yum::versionlock { '0:bash-4.1.2-9.el6_2.*':
-    ensure => present,
-  }
+   yum::versionlock { '0:bash-4.1.2-9.el6_2.*':
+     ensure       => present,
+   }
 }
