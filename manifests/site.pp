4561c993575156e0665ceabe1378090bdc5e11df
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-12-13 11:41:30 -0800

    hiera test

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 # Needed for hiera v5
 # Merge several arrays of class names into one array:
 # lookup('classes', {merge =>  'hash'})
-#hiera_include(classes)
+hiera_include(classes)
 lookup('classes', {merge =>  'hash'})
 # include lookup('classes', { 'merge' => 'unique' })
 #lookup('classes', Array[String], 'unique').include
