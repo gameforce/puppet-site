c0dd40b23f3517b199e594b677544b81fb2beb37
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-12-13 12:19:13 -0800

    The first element of the merge has wrong type, expects a Hash value, got Tuple

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,5 +3,4 @@
 # lookup('classes', {merge =>  'hash'})
 hiera_include(classes)
 lookup('classes', {merge =>  'hash'})
-# include lookup('classes', { 'merge' => 'unique' })
-#lookup('classes', Array[String], 'unique').include
+include lookup('classes', { 'merge' => 'unique' })
