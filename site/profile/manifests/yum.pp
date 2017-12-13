6a9318b86c01e187d564df36a134acbe3636ea62
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-12-13 10:20:58 -0800

    yum test

--- site/profile/manifests/yum.pp
+++ site/profile/manifests/yum.pp
@@ -6,7 +6,7 @@ class { 'yum':
     managed_repos =>  [ 'stellar' ],
   }
 
-class { 'yum::managed_repos':
+yum::managed_repos { 'stellar':
     repos     => 'stellar',
     ensure    => 'present',
     enabled   => 'true',
