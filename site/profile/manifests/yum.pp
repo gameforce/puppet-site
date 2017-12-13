baec3f481806c9bbc43d135042d74b5dbb3d4f1b
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-12-13 09:59:29 -0800

    test yum repo bit

--- site/profile/manifests/yum.pp
+++ site/profile/manifests/yum.pp
@@ -11,12 +11,12 @@ yum::versionlock { '0:bash-4.1.2-9.el6_2.*':
 }
 
 
-#  managed_repos { 'stellar':
-#    ensure    => 'present',
-#    enabled   => 'true',
-#    descr     => 'Stellar Repo',
-#    baseurl   => 'http://repo/stellar/x86_64/',
-#    gpgcheck  => false,
-#    target    => '/etc/yum.repos.d/stellar.repo'
-#  }
+  managed_repos { 'stellar':
+    ensure    => 'present',
+    enabled   => 'true',
+    descr     => 'Stellar Repo',
+    baseurl   => 'http://repo/stellar/x86_64/',
+    gpgcheck  => false,
+    target    => '/etc/yum.repos.d/stellar.repo'
+  }
 }
