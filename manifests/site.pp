73d53f0662a0726e30c8a51f084388e4ef866bce
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-12-13 10:49:33 -0800

    test hiera merge hash

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,6 @@
 # Merge several arrays of class names into one array:
 # lookup('classes', {merge =>  'hash'})
 #hiera_include(classes)
-#lookup('classes', {merge =>  'hash'})
-include lookup('classes', { 'merge' => 'unique' })
+lookup('classes', {merge =>  'hash'})
+# include lookup('classes', { 'merge' => 'unique' })
 #lookup('classes', Array[String], 'unique').include
