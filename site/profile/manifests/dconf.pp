68ad5af10729d10df1a1c40fb4887e1be45cd944
Nick Gotsinas <nick.gotsinas@box53.stellarcreative.lab>
2017-12-14 12:53:00 -0800

    not sure why escape slashes

--- site/profile/manifests/dconf.pp
+++ site/profile/manifests/dconf.pp
@@ -3,15 +3,15 @@ class profile::dconf {
 
   #notify {"NEW: testing new bits":}
   class { 'gnome3':
-    button\_power    => $gnome3::params::button_power,
+    button_power    => $gnome3::params::button_power,
     background       => undef, #/usr/share/backgrounds/smile-wallpaper.jpg
     screensaver_lock => true,
     bookmarks        => undef,
     software         => undef,
     games            => $gnome3::params::games,
-    delete\_games    => true,
-    webproxy\_mode   => $gnome3::params::webproxy_mode,
-    webproxy\_autourl=> '',
-    webproxy\_locked => true,
+    delete_games    => true,
+    webproxy_mode   => $gnome3::params::webproxy_mode,
+    webproxy_autourl=> '',
+    webproxy_locked => true,
   }
 }
