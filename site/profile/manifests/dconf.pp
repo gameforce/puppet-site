04bae278fb659fe85f4081935e7d3a7d0973da10
Nick Gotsinas <nick.gotsinas@box53.stellarcreative.lab>
2017-12-14 12:51:56 -0800

    linted

--- site/profile/manifests/dconf.pp
+++ site/profile/manifests/dconf.pp
@@ -3,15 +3,15 @@ class profile::dconf {
 
   #notify {"NEW: testing new bits":}
   class { 'gnome3':
-    button\_power     => $gnome3::params::button_power,
-    background        => undef, #/usr/share/backgrounds/smile-wallpaper.jpg
-    screensaver_lock  => true,
-    bookmarks         => undef,
-    software          => undef,
-    games             => $gnome3::params::games,
-    delete\_games     => true,
-    webproxy\_mode    => $gnome3::params::webproxy_mode,
-    webproxy\_autourl => '',
-    webproxy\_locked  => true,
+    button\_power    => $gnome3::params::button_power,
+    background       => undef, #/usr/share/backgrounds/smile-wallpaper.jpg
+    screensaver_lock => true,
+    bookmarks        => undef,
+    software         => undef,
+    games            => $gnome3::params::games,
+    delete\_games    => true,
+    webproxy\_mode   => $gnome3::params::webproxy_mode,
+    webproxy\_autourl=> '',
+    webproxy\_locked => true,
   }
 }
