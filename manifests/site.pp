46e39d4208c452f584ec80d979a8f694b329f2b2
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-12-13 16:12:54 -0800

    added lookup_options and merge strategy

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,5 +2,4 @@
 # Merge several arrays of class names into one array:
 # lookup('classes', {merge =>  'hash'})
 hiera_include(classes)
-#lookup('classes', {merge =>  'hash'})
-lookup('classes', { 'merge' => 'unique' })
+#lookup('classes', { 'merge' => 'hash' })
