42fcf69e8994fa3402e854fe7f2e5d095ac75511
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-12-13 16:52:02 -0800

    adding kmod-wacom to versionlock list

--- site/profile/manifests/yum.pp
+++ site/profile/manifests/yum.pp
@@ -2,7 +2,11 @@ class profile::yum {
 
 include 'yum'
 
-yum::versionlock { '0:kernel-3.10.0-693.5.2.el7.*':
+yum::versionlock { '0:kernel-3.10.0-693.5.2.el7':
+  ensure => present,
+  }
+
+yum::versionlock { '1:kmod-wacom-0.37.1-1.el7_4.*':
   ensure => present,
   }
 }
