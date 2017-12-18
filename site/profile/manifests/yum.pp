4e76bf53ab33d20ddcb4965e6af9eeef98f98210
Nick Gotsinas <nick.gotsinas@stellarcreativelab.com>
2017-12-18 10:16:23 -0800

    disable versionlock

--- site/profile/manifests/yum.pp
+++ site/profile/manifests/yum.pp
@@ -2,27 +2,27 @@ class profile::yum {
 
 include 'yum'
 
-yum::versionlock { '0:kernel-3.10.0-693.5.2.el7.*':
-  ensure => present,
-  }
+#yum::versionlock { '0:kernel-3.10.0-693.5.2.el7.*':
+#  ensure => present,
+#  }
 
-yum::versionlock { '0:kmod-wacom-0.37.1-1.el7_4.*':
-  ensure => present,
-  }
+#yum::versionlock { '0:kmod-wacom-0.37.1-1.el7_4.*':
+#  ensure => present,
+#  }
 
-yum::versionlock { '0:kernel-devel-643.10.0-693.11.1.el7.*':
-  ensure => present,
-  }
+#yum::versionlock { '0:kernel-devel-643.10.0-693.11.1.el7.*':
+#  ensure => present,
+#  }
 
-yum::versionlock { '0:kernel-headers-643.10.0-693.11.1.el7.*':
-  ensure => present,
-  }
+#yum::versionlock { '0:kernel-headers-643.10.0-693.11.1.el7.*':
+#  ensure => present,
+#  }
 
-yum::versionlock { '0:kernel-tools-643.10.0-693.11.1.el7.*':
-  ensure => present,
-  }
+#yum::versionlock { '0:kernel-tools-643.10.0-693.11.1.el7.*':
+#  ensure => present,
+#  }
 
-yum::versionlock { '0:kernel-tools-libs-643.10.0-693.11.1.el7.*':
-  ensure => present,
-  }
+#yum::versionlock { '0:kernel-tools-libs-643.10.0-693.11.1.el7.*':
+#  ensure => present,
+#  }
 }
