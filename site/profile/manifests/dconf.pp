230077d0cf8415737391318c2bed323d1f720e3f
Nick Gotsinas <nick.gotsinas@stellarcreative.lab>
2017-12-19 11:11:52 -0800

    desktop?

--- site/profile/manifests/dconf.pp
+++ site/profile/manifests/dconf.pp
@@ -1,7 +1,9 @@
 # Configure dconf defaults for gnome3
 class profile::dconf {
 
-  #notify {"NEW: testing new bits":}
+  # used by desktop and
+  desktop::gnome exec { 'dconf_update': command => '/usr/bin/dconf update', refreshonly => true, require => Package['dconf'], }
+  
   file { '/etc/dconf/profile/gdm':
     ensure    => 'present',
     owner     => 'root',
