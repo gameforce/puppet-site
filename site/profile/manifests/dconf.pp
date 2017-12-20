95f2efb9d0c22b3ca8022ce8df4ce6a73af5a4ef
Nick Gotsinas <nick.gotsinas@stellarcreative.lab>
2017-12-19 16:13:29 -0800

    dconf settings

--- site/profile/manifests/dconf.pp
+++ site/profile/manifests/dconf.pp
@@ -10,6 +10,7 @@ class profile::dconf {
     group     => 'root',
     mode      => '0644',
     source    => 'puppet:///files/dconf/gdm',
+    notify    => Exec['dconf update'],
   }
 
   file { '/etc/dconf/db/gdm.d/00-login-screen':
@@ -20,6 +21,14 @@ class profile::dconf {
     source    => 'puppet:///files/dconf/00-login-screen',
     notify    => Exec['dconf update'],
   }
+
+  file { '/etc/dconf/db/system.d/common':
+    ensure    => 'present',
+    owner     => 'root',
+    group     => 'root',
+    mode      => '0644',
+    source    => 'puppet:///files/dconf/common',
+    notify    => Exec['dconf update'],
 
   exec { "dconf update":
     command     => "/usr/bin/dconf update",
