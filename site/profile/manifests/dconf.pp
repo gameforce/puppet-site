175b0426352678f611b78264866a31a36482456a
Nick Gotsinas <nick.gotsinas@stellarcreative.lab>
2017-12-19 16:17:44 -0800

    remove unneeded comment

--- site/profile/manifests/dconf.pp
+++ site/profile/manifests/dconf.pp
@@ -1,9 +1,6 @@
 # Configure dconf defaults for gnome3
 class profile::dconf {
-
-  # used by desktop and
-  # desktop::gnome { 'dconf_update': command => '/usr/bin/dconf update', refreshonly => true, require => Package['dconf'], }
-
+  
   file { '/etc/dconf/profile/gdm':
     ensure    => 'present',
     owner     => 'root',
