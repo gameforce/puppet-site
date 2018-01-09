c7be9dc749c590bbfe916282ecac5fbaf311d933
Nick Gotsinas <nick.gotsinas@stellarcreative.lab>
2018-01-09 12:47:52 -0800

    remove the rv handler install from puppet as pipeline wants to deploy it via wrapper

--- files/stellar/stellar.sh
+++ files/stellar/stellar.sh
@@ -4,10 +4,10 @@
 export PATH=/net/pipeline/bin:/usr/lib64/qt-3.3/bin:/opt/puppetlabs/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
 
 # Install rv handlers
-RV_LOCATION=/net/software/rv/rv-Linux-x86-64-7.2.0/bin
-if [ ! -e $HOME/.local/share/applications/rv.desktop ]
-then
-   ${RV_LOCATION}/rv.install_handler
-   ${RV_LOCATION}/rv.install_handler_rvpush
-   echo "RV handlers installed"
-fi
+#RV_LOCATION=/net/software/rv/rv-Linux-x86-64-7.2.0/bin
+#if [ ! -e $HOME/.local/share/applications/rv.desktop ]
+#then
+   #${RV_LOCATION}/rv.install_handler
+   #${RV_LOCATION}/rv.install_handler_rvpush
+   #echo "RV handlers installed"
+#fi
