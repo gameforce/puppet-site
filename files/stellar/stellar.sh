c6726758c599f31822864fe29132c43a3f568779
Nick Gotsinas <nick.gotsinas@stellarcreative.lab>
2018-01-09 11:35:03 -0800

    set rv location

--- files/stellar/stellar.sh
+++ files/stellar/stellar.sh
@@ -4,6 +4,7 @@
 export PATH=/net/pipeline/bin:/usr/lib64/qt-3.3/bin:/opt/puppetlabs/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
 
 # Install rv handlers
+RV_LOCATION=/net/software/rv/rv-Linux-x86-64-7.2.0/bin
 if [ ! -e $HOME/.local/share/applications/rv.desktop ]
 then
    ${RV_LOCATION}/rv.install_handler
