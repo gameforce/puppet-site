cacb05c5a86f98fe229152ca852aa0286454313e
Nick Gotsinas <nick.gotsinas@stellarcreative.lab>
2018-01-12 12:01:50 -0800

    typos and clean up

--- site/profile/manifests/yum.pp
+++ site/profile/manifests/yum.pp
@@ -2,12 +2,6 @@ class profile::yum {
 
 include 'yum'
 
-#class { 'yum':
-#  keep_kernel_devel => true,
-#  clean_old_kernels => true,
-#  exclude => 'kernel*',
-#}
-
 yum::config { 'exclude':
   ensure => 'kernel*',
 }
@@ -16,23 +10,4 @@ yum::config { 'exclude':
 #  ensure => present,
 #  }
 
-#yum::versionlock { '0:kmod-wacom-0.37.1-1.el7_4.*':
-#  ensure => present,
-#  }
-
-#yum::versionlock { '0:kernel-devel-643.10.0-693.11.1.el7.*':
-#  ensure => present,
-#  }
-
-#yum::versionlock { '0:kernel-headers-643.10.0-693.11.1.el7.*':
-#  ensure => present,
-#  }
-
-#yum::versionlock { '0:kernel-tools-643.10.0-693.11.1.el7.*':
-#  ensure => present,
-#  }
-
-#yum::versionlock { '0:kernel-tools-libs-643.10.0-693.11.1.el7.*':
-#  ensure => present,
-#  }
 }
