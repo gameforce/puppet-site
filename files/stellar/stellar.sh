7a4a03962a05e1a8e722a60625c9162b1b53c657
Nick Gotsinas <nick.gotsinas@stellarcreative.lab>
2018-01-18 12:10:43 -0800

    set umask 002

--- files/stellar/stellar.sh
+++ files/stellar/stellar.sh
@@ -3,6 +3,9 @@
 # setup Stellar PATH
 export PATH=/net/pipeline/bin:/usr/lib64/qt-3.3/bin:/opt/puppetlabs/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
 
+# set file creation to rw-rw-r
+umask 002
+
 # Install rv handlers
 #RV_LOCATION=/net/software/rv/rv-Linux-x86-64-7.2.0/bin
 #if [ ! -e $HOME/.local/share/applications/rv.desktop ]
