fdc729efcc5d71a2573661ca06322bad4ccc1034
Nick Gotsinas <nick.gotsinas@stellarcreative.lab>
2018-01-18 16:06:05 -0800

    remove system.d and investigate why its not there

--- site/profile/manifests/dconf.pp
+++ site/profile/manifests/dconf.pp
@@ -19,14 +19,14 @@ class profile::dconf {
     notify    => Exec['dconf update'],
   }
 
-  file { '/etc/dconf/db/system.d/common':
-    ensure    => 'present',
-    owner     => 'root',
-    group     => 'root',
-    mode      => '0644',
-    source    => 'puppet:///files/dconf/common',
-    notify    => Exec['dconf update'],
-  }
+  #file { '/etc/dconf/db/system.d/common':
+  #  ensure    => 'present',
+  #  owner     => 'root',
+  #  group     => 'root',
+  #  mode      => '0644',
+  #  source    => 'puppet:///files/dconf/common',
+  #  notify    => Exec['dconf update'],
+  #}
 
   exec { "dconf update":
     command     => "/usr/bin/dconf update",
--- site/profile/manifests/yum.pp
+++ site/profile/manifests/yum.pp
@@ -3,7 +3,7 @@ class profile::yum {
 include 'yum'
 
 yum::config { 'exclude':
-  ensure => 'kernel*',
+  ensure => 'kernel.*',
 }
 
 #yum::versionlock { '0:kernel-3.10.0-693.5.2.el7.*':
