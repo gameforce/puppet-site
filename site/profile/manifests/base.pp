b79b9446b31de1f5800aee60e67053747ffed420
Nick <nick.gotsinas@gmail.com>
2018-07-11 13:44:12 -0700

    ldap_id_mapping = false

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -77,7 +77,7 @@ class profile::base {
       'access_provider'                => 'ad',
       'krb5_store_password_if_offline' => 'True',
       'default_shell'                  => '/bin/bash',
-      'ldap_id_mapping'                => true,
+      'ldap_id_mapping'                => false,
       'enumerate'                      => true,
       'fallback_homedir'               => '/home/%u',
       },
