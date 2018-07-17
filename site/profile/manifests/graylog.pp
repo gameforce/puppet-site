d1f6d06e98095ae20eb4ced645da8ccefed45e4a
Nick <nick.gotsinas@gmail.com>
2018-07-17 14:19:03 -0700

    requries mod 'elastic-elastic_stack', '6.2.0'

--- site/profile/manifests/graylog.pp
+++ site/profile/manifests/graylog.pp
@@ -6,12 +6,13 @@ class profile::graylog {
   class { 'mongodb::server':
      bind_ip => ['127.0.0.1'],
   }
+  # requires mod 'elastic-elastic_stack', '6.2.0'
   class { 'elastic_stack::repo':
   version => 5,
-  }
-  class { 'elasticsearch':
-    version      => '5.5.1',
-  }
+ }
+ class { 'elasticsearch':
+   version      => '5.5.1',
+ }
     elasticsearch::instance { 'graylog':
     config => {
       'cluster.name' => 'graylog',
