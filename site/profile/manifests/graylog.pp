3899c6f5b40416c4f66ec5699824c5738cc946ec
Nick <nick.gotsinas@gmail.com>
2018-07-17 14:49:39 -0700

    set network host IP

--- site/profile/manifests/graylog.pp
+++ site/profile/manifests/graylog.pp
@@ -19,7 +19,7 @@ class profile::graylog {
 elasticsearch::instance { 'graylog':
   config => {
     'cluster.name' => 'graylog',
-    'network.host' => '127.0.0.1',
+    'network.host' => '192.168.10.13',
   }
 }
 
