d8b1754da72fa82b207b52b5ec59a6e2b050e347
Nick <nick.gotsinas@gmail.com>
2018-07-16 17:44:48 -0700

    inlcude dotfiles repo

--- site/profile/manifests/accounts.pp
+++ site/profile/manifests/accounts.pp
@@ -21,21 +21,14 @@ class profile::accounts {
     groups           => [ 'users','puppet' ]
   }
 
-  # ssh key we created on the puppet master during the install
-  file { '/root/.ssh/id_rsa.pub':
-    ensure  =>  'present',
-    owner   =>  'root',
-    group   =>  '0',
-    mode    =>  '0600',
-    source  =>  'puppet:///files/ssh/id_rsa.pub'
-  }
-
-  ssh_authorized_key { 'r10k@owi.lan':
-    ensure =>  present,
-    user   =>  'root',
-    type   =>  'ssh-rsa',
-    key    =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQDQJtxyjVtzJ6Nuulm86e17kRfvclYsZhzpfi/UtBpicDJaZAgeC0LWZC7/x/vOLkA256QbxaNouQTblWLzel7lJqaq2iZ6o08e2vFGf1t9Pw69bBZN4pBXnHvyKKECjdSmatnaYQCOfB2QiPsm0RsU72yIBpBKW7/a/Yw7ecuwvlpNXrDJ5l78DjCt1g+f8W3eW/caOW+4XhmeIm+mV66F0PZz4Zddxeu4aByqJ+hs11VSt3jEjh9B/FuMPsH1Wae2+8nMAeZVl5oN7/X3hy96QaAFh3e/x22TdUqHHbCBWZWOhyftrfITOH7SY8HmcHFFjVhhPG3PIxqosN6XUQlD',
-    name   =>  'r10k@site',
+  # clone our dotfiles repo
+  vcsrepo { '/home/systems/.dotfiles':
+    ensure   => 'latest',
+    provider => 'git',
+    owner    => 'systems',
+    group    => 'systems',
+    source   => 'https://github.com/gameforce/dotfiles.git',
+    revision => 'master',
   }
   # Setup symlinks to dotfiles
   file { '/home/systems/.zshrc':
@@ -61,5 +54,22 @@ class profile::accounts {
   file { '/home/systems/.screenrc':
     ensure => 'symlink',
     target => '/home/systems/.dotfiles/screenrc',
+  }
+
+  # ssh key we created on the puppet master during the install
+  file { '/root/.ssh/id_rsa.pub':
+    ensure  =>  'present',
+    owner   =>  'root',
+    group   =>  '0',
+    mode    =>  '0600',
+    source  =>  'puppet:///files/ssh/id_rsa.pub'
+  }
+
+  ssh_authorized_key { 'r10k@owi.lan':
+    ensure =>  present,
+    user   =>  'root',
+    type   =>  'ssh-rsa',
+    key    =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQDQJtxyjVtzJ6Nuulm86e17kRfvclYsZhzpfi/UtBpicDJaZAgeC0LWZC7/x/vOLkA256QbxaNouQTblWLzel7lJqaq2iZ6o08e2vFGf1t9Pw69bBZN4pBXnHvyKKECjdSmatnaYQCOfB2QiPsm0RsU72yIBpBKW7/a/Yw7ecuwvlpNXrDJ5l78DjCt1g+f8W3eW/caOW+4XhmeIm+mV66F0PZz4Zddxeu4aByqJ+hs11VSt3jEjh9B/FuMPsH1Wae2+8nMAeZVl5oN7/X3hy96QaAFh3e/x22TdUqHHbCBWZWOhyftrfITOH7SY8HmcHFFjVhhPG3PIxqosN6XUQlD',
+    name   =>  'r10k@site',
   }
 }
