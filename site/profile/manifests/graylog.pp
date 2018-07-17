1164d4a484d08d970a64ed3a9c4131da52a4d669
Nick <nick.gotsinas@gmail.com>
2018-07-17 14:40:24 -0700

    turn on graylog web

--- site/profile/manifests/graylog.pp
+++ site/profile/manifests/graylog.pp
@@ -32,10 +32,16 @@ elasticsearch::instance { 'graylog':
     version => '2.4'
   }->
   class { 'graylog::server':
-    package_version => '2.4.0-9',
-    config          => {
-      'password_secret' => 'glpass0',    # Fill in your password secret
-      'root_password_sha2' => '78A8FEE4F288990B5D0C27DDB5B6D36B5937E2ABFE11F2F6BCE99052F4DAB89C', # Fill in your root password hash
+    package_version           => '2.4.0-9',
+    config                    => {
+      'password_secret'       => 'glpass0',    # Fill in your password secret
+      'root_password_sha2'    => '78A8FEE4F288990B5D0C27DDB5B6D36B5937E2ABFE11F2F6BCE99052F4DAB89C', # Fill in your root password hash
+      'web_enable'            => true,
+      'web_listen_uri'        => 'http://graylog.owi.lan:9000/',
+      'web_enable_tls'        => false,
+      'web_tls_cert_file'     => '/etc/ssl/graylog/graylog_cert_chain.crt',
+      'web_tls_key_file'      => '/etc/ssl/graylog/graylog_key_pkcs8.pem',
+      'web_tls_key_password'  => 'sslkey-password',
      }
   }
 }
