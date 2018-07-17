f7782f5625b7ba8e34aae917d96717cc75dd0402
Nick <nick.gotsinas@gmail.com>
2018-07-17 15:31:10 -0700

    set the endpoint uri

--- site/profile/manifests/graylog.pp
+++ site/profile/manifests/graylog.pp
@@ -36,6 +36,8 @@ elasticsearch::instance { 'graylog':
     config                    => {
       'password_secret'       => '78A8FEE4F288990B5D0C27DDB5B6D36B5937E2ABFE11F2F6BCE99052F4DAB89C',    # Fill in your password secret
       'root_password_sha2'    => '78A8FEE4F288990B5D0C27DDB5B6D36B5937E2ABFE11F2F6BCE99052F4DAB89C', # Fill in your root password hash
+      'rest_listen_uri'       => 'http://graylog.owi.lan:9000/api/',
+      'rest_transport_uri'    => 'http://graylog.owi.lan:9000/api/',
       'web_enable'            => true,
       'web_listen_uri'        => 'http://graylog.owi.lan:9000/',
       'web_enable_tls'        => false,
