0a1a8dc4f2b24fa307abf869a9e13ee50e4aa60c
Nick <nick.gotsinas@gmail.com>
2018-07-16 17:42:57 -0700

    setup systems account dotfiles

--- site/profile/manifests/accounts.pp
+++ site/profile/manifests/accounts.pp
@@ -37,4 +37,29 @@ class profile::accounts {
     key    =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQDQJtxyjVtzJ6Nuulm86e17kRfvclYsZhzpfi/UtBpicDJaZAgeC0LWZC7/x/vOLkA256QbxaNouQTblWLzel7lJqaq2iZ6o08e2vFGf1t9Pw69bBZN4pBXnHvyKKECjdSmatnaYQCOfB2QiPsm0RsU72yIBpBKW7/a/Yw7ecuwvlpNXrDJ5l78DjCt1g+f8W3eW/caOW+4XhmeIm+mV66F0PZz4Zddxeu4aByqJ+hs11VSt3jEjh9B/FuMPsH1Wae2+8nMAeZVl5oN7/X3hy96QaAFh3e/x22TdUqHHbCBWZWOhyftrfITOH7SY8HmcHFFjVhhPG3PIxqosN6XUQlD',
     name   =>  'r10k@site',
   }
+  # Setup symlinks to dotfiles
+  file { '/home/systems/.zshrc':
+    ensure => 'symlink',
+    target => '/home/systems/.dotfiles/zshrc.linux',
+  }
+
+  file { '/home/systems/.vimrc':
+    ensure => 'symlink',
+    target => '/home/systems/.dotfiles/vimrc',
+  }
+
+  file { '/home/systems/.vim':
+    ensure => 'symlink',
+    target => '/home/systems/.dotfiles/vim',
+  }
+
+  file { '/home/systems/.gitconfig':
+    ensure => 'symlink',
+    target => '/home/systems/.dotfiles/gitconfig',
+  }
+
+  file { '/home/systems/.screenrc':
+    ensure => 'symlink',
+    target => '/home/systems/.dotfiles/screenrc',
+  }
 }
