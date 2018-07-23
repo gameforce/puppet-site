1063df6fb25b9bfdf5ba82f1541bc3b1adf79dc5
Nick <nick.gotsinas@gmail.com>
2018-07-23 14:44:28 -0700

    missed a comma

--- site/profile/manifests/docker.pp
+++ site/profile/manifests/docker.pp
@@ -9,7 +9,7 @@ class profile::docker {
 
   docker::run { 'sickrage':
   image   => 'sickrage',
-  image_tag => 'sickrage'
+  image_tag => 'sickrage',
   detach  => true,
   command => '',
   volumes => ['config:/opt/sickrage/config', 'downloads:/net/systems/downloads', 'series:/net/series', 'localtime:/etc/localtime:ro',],
