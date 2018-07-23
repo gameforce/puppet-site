26e609e752228f2ed582375763f01fa51e8e87ee
Nick <nick.gotsinas@gmail.com>
2018-07-23 12:18:20 -0700

    docker sickrage pull test

--- site/profile/manifests/docker.pp
+++ site/profile/manifests/docker.pp
@@ -1,4 +1,14 @@
 # Installs docker
 class profile::docker {
   include 'docker'
+
+  docker::run { 'sickrage':
+  image   => 'sickrage:sickrage',
+  command => '',
+  volumes => ['config:/opt/sickrage/config'],
+  volumes => ['downloads:/net/systems/downloads'],
+  volumes => ['series:/net/series'],
+  volumes => ['localtime:/etc/localtime:ro'],
+  ports   => ['8081', '8081'],
+  }
 }
